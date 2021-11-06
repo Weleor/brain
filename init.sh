92eaca3416cbcabac5c96db7e212e74537344aa9
Ilya <weleor@icloud.com>
2021-11-06 12:59:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,18 +4,21 @@ wget https://bitkeys.work/btc_balance_sorted.csv
 
 mkdir /root/BTC & cd /root/BTC
 echo " **************** Download brainflayer **************** "
-git clone https://github.com/matchv/brainflayer.git
+git clone https://github.com/ryancdotorg/brainflayer.git
 
-echo " **************** Download Bip39Helper **************** "
-
-wget https://github.com/TheRealLordFractal/Bip39Helper/archive/refs/tags/1.02a.zip
-echo " **************** unzip Bip39Helper **************** "
-unzip 1.02a.zip
-echo " **************** delete 1.02a.zip **************** "
-rm 1.02a.zip
 echo " **************** download apt libs **************** "
-apt-get install openssl libgmp3-dev libimobiledevice-dev libplist-dev libusbmuxd-dev libssl-dev zlib1g-dev dh-autoreconf
+apt-get install -y openssl libgmp3-dev libimobiledevice-dev libplist-dev libusbmuxd-dev libssl-dev zlib1g-dev dh-autoreconf
+apt install build-essential libgmp-dev libssl-dev
+apt install python
 
+echo " **************** download wordlist generator **************** "
 cd /root/BTC/brainflayer/
+wget https://raw.githubusercontent.com/Weleor/brain/main/brain12words.py
+wget https://raw.githubusercontent.com/Weleor/brain/main/wordlist.txt
+chmod +x *
 make
+echo " **************** test  make result **************** "
+
 ./brainflayer -h
+
+screen ./brain12words.py -n 9000000000000000000 -l 12 -w brainwalletdictionary.txt | ./brainflayer -v -o found.txt -b bloom.blf
